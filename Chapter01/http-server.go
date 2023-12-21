31b569afdf6e013a6d1c5b995a3a0ff931807a78
Jonas Gacrama <jonasgacrama@gmail.com>
2023-12-21 20:41:29 +0800

    Chapter01: Added an informational message

--- Chapter01/http-server.go
+++ Chapter01/http-server.go
@@ -17,6 +17,7 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", helloWorld)
+	fmt.Printf("Open http://%s:%s/ in your browser.\nClick CTRL + C to exit", CONN_HOST, CONN_PORT)
 	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
 	if err != nil {
 		log.Fatal("error starting http server : ", err)
