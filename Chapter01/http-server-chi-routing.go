bba69a2c0e47ae547f17901cf92fc5debe916855
Jonas Gacrama <jonasgacrama@gmail.com>
2023-12-23 20:32:21 +0800

    Chapter01: Refactored to use Chi MUX

--- Chapter01/http-server-chi-routing.go
+++ Chapter01/http-server-chi-routing.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
@@ -10,24 +11,26 @@ const (
 	CONN_PORT = "8080"
 )
 
-var GetRequestHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func GetRequestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
-})
+}
 
-var PostRequestHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func PostRequestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("It's a Post Request!"))
-})
+}
 
-var PathVariableHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["name"]
+func PathVariableHandler(w http.ResponseWriter, r *http.Request) {
+	name := chi.URLParam(r, "name")
 	w.Write([]byte("Hi " + name))
-})
+}
 
 func main() {
-	router := mux.NewRouter()
-	router.Handle("/", GetRequestHandler).Methods("GET")
-	router.Handle("/post", PostRequestHandler).Methods("POST")
-	router.Handle("/hello/{name}", PathVariableHandler).Methods("GET", "PUT")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", GetRequestHandler)
+	router.Post("/post", PostRequestHandler)
+	router.Put("/hello/{name}", PathVariableHandler)
+
 	http.ListenAndServe(CONN_HOST+":"+CONN_PORT, router)
 }
